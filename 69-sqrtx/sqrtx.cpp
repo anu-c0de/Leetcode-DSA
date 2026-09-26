69cedfc7b5f3bca9ad2b8e1d17c4db92a8244366
anu-c0de <anuragkumarskumars@gmail.com>
2026-09-26 20:09:23 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (13.71%) - LeetSync

--- 69-sqrtx/sqrtx.cpp
+++ 69-sqrtx/sqrtx.cpp
@@ -5,9 +5,10 @@ public:
         int hi = x;
         while(lo<=hi){
             int mid = lo + (hi-lo)/2;
-            long long sq = 1LL * mid * mid;
-            if( sq == x) return mid;
-            else if(sq >x) hi = mid-1;
+            long long m = (long long)mid;
+            long long y = (long long)x;
+            if(( m*m)==y) return mid;
+            else if(m*m > y) hi = mid-1;
             else lo = mid+1;
         }
         return hi;
